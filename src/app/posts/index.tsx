2a1c261b658f8f2af0cb8d0c647c0ecd758aea2e
paopapaopao <paolo.casugay@gmail.com>
2026-05-20 21:34:27 +0800

    Add link to Post page

--- src/app/posts/index.tsx
+++ src/app/posts/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
@@ -71,13 +72,14 @@ const PostsPage = () => {
       <FlatList
         data={posts}
         renderItem={({ item: post }) => (
-          <View
+          <Link
             style={styles.card}
             key={post.id}
+            href={`/posts/${post.id}`}
           >
             <Text style={styles.title}>{post.title}</Text>
             <Text style={styles.body}>{post.body}</Text>
-          </View>
+          </Link>
         )}
         ItemSeparatorComponent={<View style={styles.spacer} />}
         ListHeaderComponent={<Text style={styles.header}>Post List</Text>}
