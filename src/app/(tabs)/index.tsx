fa1c7a0dc2e1a2ddf081ca7a619b39fc55077553
paopapaopao <paolo.casugay@gmail.com>
2026-05-23 18:32:04 +0800

    Implement scroll to top

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { MaterialDesignIcons } from '@react-native-vector-icons/material-design-icons';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { Link } from 'expo-router';
+import { useRef, useState } from 'react';
 import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { PostCard, PostCardSkeleton } from '@/components';
@@ -97,6 +98,9 @@ const HomePage = () => {
     },
   });
 
+  const [isShown, setIsShown] = useState(false);
+  const flatListRef = useRef<FlatList<Post>>(null);
+
   if (isPending) {
     return (
       <FlatList
@@ -122,6 +126,12 @@ const HomePage = () => {
   return (
     <View style={styles.page}>
       <FlatList
+        ref={flatListRef}
+        onScroll={(event) => {
+          const offSetY = event.nativeEvent.contentOffset.y;
+
+          setIsShown(offSetY > 400);
+        }}
         data={posts}
         renderItem={({ item: post }) => (
           <Link
@@ -160,16 +170,20 @@ const HomePage = () => {
         refreshing={isRefetching}
         onRefresh={refetch}
       />
-      <Pressable
-        style={styles.fab}
-        onPress={() => {}}
-      >
-        <MaterialDesignIcons
-          name='arrow-up'
-          size={24}
-          color='white'
-        />
-      </Pressable>
+      {isShown && (
+        <Pressable
+          style={styles.fab}
+          onPress={() => {
+            flatListRef.current?.scrollToOffset({ offset: 0, animated: true });
+          }}
+        >
+          <MaterialDesignIcons
+            name='arrow-up'
+            size={24}
+            color='white'
+          />
+        </Pressable>
+      )}
     </View>
   );
 };
